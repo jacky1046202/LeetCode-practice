a196498cd7963c8d849b228190658b42e2df9beb
jacky1046202 <jacky1046202@gmail.com>
2026-02-26 19:50:58 +0800

    26/2/26

---
+++ 1404_Number of Steps to Reduce a Number in Binary Representation to One.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    private void divideByTwo(StringBuilder s) {
+        s.deleteCharAt(s.length() - 1);
+    }
+
+    private void addOne(StringBuilder s) {
+        int i = s.length() - 1;
+
+        //i會從最右到出現0，如果全都是1，i就會是-1
+        while (i >= 0 && s.charAt(i) != '0') {
+            s.setCharAt(i, '0');
+            i--;
+        }
+
+        //處理進位
+        if (i < 0) {
+            s.insert(0, '1');
+        } else {
+            s.setCharAt(i, '1');
+        }
+    }
+
+    public int numSteps(String s) {
+        StringBuilder str = new StringBuilder(s);
+
+        int operations = 0;
+        while (str.length() > 1) {
+            int N = str.length();
+
+            if (str.charAt(N - 1) == '0') {
+                divideByTwo(str);
+            } else {
+                addOne(str);
+            }
+
+            operations++;
+        }
+
+        return operations;
+    }
+}
